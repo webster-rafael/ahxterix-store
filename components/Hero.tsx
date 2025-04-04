f8013bf1796e9cec2b8a8ab71d1eff489d2b23b1
webster-rafael <webster.rafael2014@gmail.com>
2025-04-04 02:47:30 -0400

    feat: adicionar fundo responsivo ao componente Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <section className="w-full bg-heroMobile 2xl:bg-heroDesktop bg-contain bg-no-repeat bg-bottom h-[650px] xl:h-[600px] 2xl:h-[900px]"></section>
+    <section className="w-full bg-heroMobile lg:bg-heroNotebook 2xl:bg-heroDesktop bg-contain bg-no-repeat bg-bottom h-[650px] xl:h-[600px] 2xl:h-[900px]"></section>
   );
 };
 
